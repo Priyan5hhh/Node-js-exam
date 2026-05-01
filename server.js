e93f3bc4618bcb4aff7ca3fd7509c8b3f7da8c5d
Priyansh Rajput <rajput11priyanshal@gmail.com>
2026-05-01 11:47:06 +0530

    Final commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http')
+
+const data = [];
+let i = 0;
+
+const backup = [];
+
+const server = http.createServer('/', (req, res) => {
+    if(req.url === '/updateUser' && req.method === 'GET'){
+        console.log(time)
+        data[i++] = "User " + i;
+    } else if(req.url === '/saveLog' && req.method === 'GET'){
+        console.log(data);
+    } else if(req.url === '/backup' && req.method === 'POST'){
+       backup = data;  
+    } else if(req.url === '/clearLog' && req.method === 'GET') {
+        data = [];
+    } else if(req.url === '/serverInfo' && req.method === 'GET') {
+        console.log({data});
+    } else console.log("Welcome to Home Page")
+}).listen(3000)
+
